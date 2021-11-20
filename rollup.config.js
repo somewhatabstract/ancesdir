fae70ca37c73c9241660787bb243c7639b53a831
Jeff Yates <jeff@khanacademy.org>
2021-11-20 11:34:38 -0600

    Update eslint, babel, etc. and add esm output

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,24 @@ import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import {terser} from "rollup-plugin-terser";
-import analyzer from "rollup-plugin-analyzer";
-import visualizer from "rollup-plugin-visualizer";
-
-const getOptionalPlugins = () => {
-    if (process.env.NODE_ENV === "CI") {
-        // We don't need any of these when running on CI.
-        return [];
-    }
-
-    return [
-        // NOTE: The analysis is of the pre-minified output.
-        // So the reported bundle size is the non-minified size that includes
-        // comments and full code.
-        analyzer({summaryOnly: true, filter: (module) => module.size !== 0}),
-        visualizer({
-            title: "ancesdir bundle rollup (minified)",
-            filename: "obj/stats.html",
-        }),
-    ];
-};
+import filesize from "rollup-plugin-filesize";
 
 export default {
     input: "./src/index.js",
-    output: {
-        file: "./dist/index.js",
-        format: "cjs",
-        sourcemap: true,
-    },
+    output: [
+        {
+            file: "./dist/index.js",
+            format: "cjs",
+            sourcemap: true,
+            exports: "auto",
+        },
+        {
+            file: "./dist/es/index.js",
+            format: "esm",
+            sourcemap: true,
+            exports: "auto",
+        },
+    ],
     plugins: [
         resolve({preferBuiltins: true}),
         babel({
@@ -38,6 +28,6 @@ export default {
         }),
         commonjs(),
         terser(),
-        ...getOptionalPlugins(),
+        filesize(),
     ],
 };
