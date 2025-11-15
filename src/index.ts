32cadff10d457070055ad5eb92f7412445137bce
Jeff Yates <jeff@somewhatabstract.com>
2025-11-15 17:58:33 -0600

    [provenance.1] Add named export for ancesdir and set default export as deprecated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export {ancesdir as default} from "./ancesdir";
+import {ancesdir} from "./ancesdir";
+/**
+ * @deprecated Use named import instead: `import {ancesdir} from "ancesdir";`
+ * The default export will be removed in a future major release.
+ */
+const defaultExport = ancesdir;
+export default defaultExport;
+
+export {ancesdir} from "./ancesdir";
 export {closesdir} from "./closesdir";
 export {clearCache} from "./cache";
