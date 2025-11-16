ce7ca3660b1ea5d720d6310eacb148284c5b8783
Jeff Yates <jeff@somewhatabstract.com>
2025-11-15 19:01:06 -0600

    [provenance.1] Add some commentary to explain why we handle the default export the way we do

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import {ancesdir} from "./ancesdir";
+
+// TODO: Delete the default export in the next major release.
+// NOTE: Needed to import and assign to a variable for the deprecation to
+// be picked up properly; it didn't work when applied to
+// the export statement directly.
 /**
  * @deprecated Use named import instead: `import {ancesdir} from "ancesdir";`
  * The default export will be removed in a future major release.
@@ -6,6 +11,6 @@ import {ancesdir} from "./ancesdir";
 const defaultExport = ancesdir;
 export default defaultExport;
 
-export {ancesdir} from "./ancesdir";
+export {ancesdir};
 export {closesdir} from "./closesdir";
 export {clearCache} from "./cache";
